14d0c3cbf76ad6d662459d935b68934c7d58330a
Loana Albisser <loana.albisser@bbv.ch>
2018-03-09 08:20:49 +0100

    add model/ service class

---
+++ Tasky/Tasky/Tasky/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tasky
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string IsCompleted { get; set; }
+    }
+}
---
+++ Tasky/Tasky/Tasky/TaskService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasky
+{
+    public class TaskService
+    {
+        private readonly List<Task> _taskList;
+
+        public TaskService()
+        {
+            _taskList = new List<Task>();
+        }
+
+        public List<Task> LoadTasks()
+        {
+            return _taskList;
+        }
+
+        public Task CreateTask()
+        {
+            var newTask = new Task();
+            var nextId = _taskList.Select(task => task.Id).DefaultIfEmpty().Max() + 1;
+
+            newTask.Id = nextId;
+            return newTask;
+        }
+
+        public Task GetTask(int taskId)
+        {
+            return _taskList.SingleOrDefault(task => task.Id == taskId);
+        }
+
+        public void AddTask(Task task)
+        {
+            _taskList.Add(task);
+        }
+    }
+}
